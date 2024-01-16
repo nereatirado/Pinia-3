a62994fafe48815e15c7fabc721c6a8dd818e4df
nerea <nerea@ubuntu.myguest.virtualbox.org>
2024-01-16 19:26:24 +0100

    pinia 2

---
+++ src/stores/CartStore.js
@@ -0,0 +1,24 @@
+import { defineStore } from "pinia";
+import { groupBy } from "lodash";
+
+export const useCartStore = defineStore("CartStore", {
+    state: () => {
+        return {
+            items: [],
+        };
+    },
+    actions:{
+        addItems(count,item){
+            count=parseInt(count);
+            for(let index=0;index<count;index++){
+                this.items.push({...item});
+            }
+        }
+    },
+    getters:{
+        count:(state)=>state.items.length,
+        isEmpty:(state)=>state.count===0,
+        grouped:state =>groupBy(state.items,item=>item.name)
+    }
+});
+
